b3170f3aaaf74aee62fbaa4c7822986c55cb5f36
AbsolucionArtistica <agustin.bahamondesc@gmail.com>
2023-02-23 12:40:52 +0000

    actualizacion1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)]; //Elige un "who" al azar
+  let randomWhat = what[Math.floor(Math.random() * what.length)]; //Elige un "what" al alzar
+  let randomWhen = when[Math.floor(Math.random() * when.length)]; //Elige un "when" al alzar
+
+  let excuseGenrator =
+    randomWho + " " + randomWhat + " my homework " + randomWhen;
+
+  const finalExcuse = document.createElement("h1");
+  finalExcuse.innerHTML = excuseGenrator;
+  document.querySelector(".excuse");
+  excuse.appendChild(finalExcuse);
 };
